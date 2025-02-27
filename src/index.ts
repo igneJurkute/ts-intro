2c2db102545047984dad2816b2c6d9378c5fec14
igneJurkute <127085677+igneJurkute@users.noreply.github.com>
2025-02-27 22:07:36 +0200

    new commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+console.clear();
+// const x = 5;
+// const y = 'labas';
+// const o = true;
+// const users = ['a', 'b'];
+// function sum(a, b){
+//     return a + b;
+// }
+
+// console.log(sum(7,5));
+/** Tiesiog skaicius... */
+const a: number = 5;
+const b: string = 'ABC';
+const c: boolean = true;
+
+/**
+ * Dvieju skaiciu sumavimas
+ * 
+ * @param a Pirmas skaicius.
+ * @param b Antras skaicius.
+ * @returns Suma.
+ */
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+const marks = [10, 2, 8, 4, 6];
+marks.push(5);
+marks.push(7, 9, 1, 3);
+
+console.log(marks);
+
+const words: string[] = [];
+// const words = [] as string[];
+// words.push(1);
+words.push('1');
+//words.push = (true);
+
+console.log(words);
+
+
+
+
